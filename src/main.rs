5bb0b5584850bd0037bae723b84816dbfd43cd11
Lina Butler <lina@yakshaving.ninja>
2025-11-25 19:54:11 -0800

    refactor: Clean up implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,54 @@
-use anyhow::anyhow;
-use bollard::{
-    Docker,
-    query_parameters::{EventsOptionsBuilder, InspectContainerOptions},
-};
 use std::{
     collections::HashMap,
     env,
     fs::{self, File},
     path::Path,
 };
+
+use anyhow::{Context, anyhow};
+use bollard::{
+    Docker,
+    query_parameters::{EventsOptionsBuilder, InspectContainerOptions},
+};
 use tokio::time::{Duration, sleep};
-use tokio_postgres::{Client, NoTls};
+use tokio_postgres::{Client, Config, NoTls};
 use tokio_stream::StreamExt;
 
 const HEALTHCHECK_FILE: &str = "/healthcheck/manager-ready";
 
-async fn connect_to_master() -> Client {
-    let citus_host = env::var("CITUS_HOST").unwrap_or_else(|_| "master".to_string());
-    let postgres_pass = env::var("POSTGRES_PASSWORD").unwrap_or_default();
-    let postgres_user = env::var("POSTGRES_USER").unwrap_or_else(|_| "postgres".to_string());
-    let postgres_db = env::var("POSTGRES_DB").unwrap_or_else(|_| postgres_user.clone());
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> anyhow::Result<()> {
+    if Path::new(HEALTHCHECK_FILE).exists() {
+        let _ = fs::remove_file(HEALTHCHECK_FILE);
+    }
 
-    let connection_string = format!(
-        "host={} user={} password={} dbname={}",
-        citus_host, postgres_user, postgres_pass, postgres_db
-    );
+    tokio::select! {
+        result = run() => result?,
 
-    loop {
-        match tokio_postgres::connect(&connection_string, NoTls).await {
-            Ok((client, connection)) => {
-                tokio::spawn(async move {
-                    if let Err(e) = connection.await {
-                        eprintln!("connection error: {}", e);
-                    }
-                });
-                eprintln!("connected to {}", citus_host);
-                return client;
-            }
-            Err(e) => {
-                eprintln!(
-                    "Could not connect to {}, trying again in 1 second: {}",
-                    citus_host, e
-                );
-                sleep(Duration::from_secs(1)).await;
-            }
+        _ = tokio::signal::ctrl_c() => {
+            eprintln!("shutting down...");
         }
     }
-}
 
-async fn add_worker(client: &Client, host: &str) -> Result<(), tokio_postgres::Error> {
-    eprintln!("adding {}", host);
-    client
-        .execute("SELECT master_add_node($1, 5432)", &[&host])
-        .await?;
     Ok(())
 }
 
-async fn remove_worker(client: &Client, host: &str) -> Result<(), tokio_postgres::Error> {
-    eprintln!("removing {}", host);
-    client
-        .batch_execute(&format!(
-            "DELETE FROM pg_dist_placement WHERE groupid = (
-                SELECT groupid FROM pg_dist_node
-                WHERE nodename = '{host}' AND nodeport = 5432 LIMIT 1
-            );
-            SELECT master_remove_node('{host}', 5432)",
-            host = host,
-        ))
-        .await?;
-    Ok(())
-}
-
-async fn docker_checker() -> anyhow::Result<()> {
+async fn run() -> anyhow::Result<()> {
     let docker = Docker::connect_with_unix_defaults()?;
-
-    let client = connect_to_master().await;
+    let mut db = connect().await;
 
     let my_hostname = env::var("HOSTNAME")?;
     let container = docker
         .inspect_container(&my_hostname, None::<InspectContainerOptions>)
-        .await?;
+        .await
+        .context(format!("couldn't fetch container `{my_hostname}`"))?;
 
     let compose_project = container
         .config
         .and_then(|config| Some(config.labels?.get("com.docker.compose.project")?.clone()))
-        .ok_or_else(|| anyhow!("Could not find Compose project label"))?;
+        .ok_or_else(|| anyhow!("container `{my_hostname}` missing Compose project label"))?;
 
-    eprintln!("found Compose project: {}", compose_project);
+    eprintln!("found Compose project: `{compose_project}`");
 
     let filters = HashMap::from([
         (
@@ -96,7 +58,7 @@ async fn docker_checker() -> anyhow::Result<()> {
         (
             "label".to_owned(),
             vec![
-                format!("com.docker.compose.project={}", compose_project),
+                format!("com.docker.compose.project={compose_project}"),
                 "com.citusdata.role=Worker".to_owned(),
             ],
         ),
@@ -110,54 +72,90 @@ async fn docker_checker() -> anyhow::Result<()> {
     }
     File::create(HEALTHCHECK_FILE)?;
 
-    let events_options = EventsOptionsBuilder::default().filters(&filters).build();
+    let mut events = docker.events(Some(
+        EventsOptionsBuilder::default().filters(&filters).build(),
+    ));
 
-    let mut events = docker.events(Some(events_options));
+    while let Some(event) = events.try_next().await? {
+        let action = match event.action {
+            Some(action) => action,
+            None => continue,
+        };
 
-    while let Some(event_result) = events.next().await {
-        match event_result {
-            Ok(event) => {
-                let worker_name = event
-                    .actor
-                    .and_then(|actor| Some(actor.attributes?.get("name")?.clone()))
-                    .unwrap_or_default();
+        let worker = match event
+            .actor
+            .and_then(|actor| Some(actor.attributes?.get("name")?.clone()))
+        {
+            Some(worker) => worker,
+            None => continue,
+        };
 
-                let action = event.action.unwrap_or_default();
+        let result = match action.as_str() {
+            "health_status: healthy" => add_worker(&db, &worker).await,
+            "destroy" => remove_worker(&mut db, &worker).await,
+            _ => continue,
+        };
 
-                let result = match action.as_str() {
-                    "health_status: healthy" => add_worker(&client, &worker_name).await,
-                    "destroy" => remove_worker(&client, &worker_name).await,
-                    _ => Ok(()),
-                };
-
-                if let Err(e) = result {
-                    eprintln!("Error processing event: {}", e);
-                }
-            }
-            Err(e) => {
-                eprintln!("Error receiving event: {}", e);
-            }
+        if let Err(err) = result {
+            eprintln!("error processing event: {err}");
         }
     }
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main() {
-    if Path::new(HEALTHCHECK_FILE).exists() {
-        let _ = fs::remove_file(HEALTHCHECK_FILE);
+async fn connect() -> Client {
+    let host = env::var("CITUS_HOST").unwrap_or_else(|_| "master".to_owned());
+    let user = env::var("POSTGRES_USER").unwrap_or_else(|_| "postgres".to_owned());
+    let password = env::var("POSTGRES_PASSWORD").ok();
+    let db = env::var("POSTGRES_DB").unwrap_or_else(|_| user.clone());
+
+    let mut config = Config::new();
+    config.host(&host).user(user).dbname(db);
+    if let Some(password) = password {
+        config.password(password);
     }
 
-    tokio::select! {
-        result = docker_checker() => {
-            if let Err(e) = result {
-                eprintln!("Error: {}", e);
-                std::process::exit(1);
+    loop {
+        match config.connect(NoTls).await {
+            Ok((client, connection)) => {
+                let _guard = tokio::spawn(async move {
+                    if let Err(err) = connection.await {
+                        eprintln!("connection error: {err}");
+                    }
+                });
+                eprintln!("connected to `{host}`");
+                return client;
+            }
+            Err(err) => {
+                eprintln!("couldn't connect to `{host}`, trying again in 1 second: {err}");
+                sleep(Duration::from_secs(1)).await;
             }
         }
-        _ = tokio::signal::ctrl_c() => {
-            eprintln!("shutting down...");
-        }
     }
+}
+
+async fn add_worker(client: &Client, host: &str) -> anyhow::Result<()> {
+    eprintln!("adding `{host}`");
+    client
+        .execute("SELECT master_add_node($1, 5432)", &[&host])
+        .await?;
+    Ok(())
+}
+
+async fn remove_worker(client: &mut Client, host: &str) -> anyhow::Result<()> {
+    eprintln!("removing `{host}`");
+    let tx = client.transaction().await?;
+    tx.execute(
+        "DELETE FROM pg_dist_placement WHERE groupid = (
+             SELECT groupid FROM pg_dist_node
+             WHERE nodename = $1 AND nodeport = 5432 LIMIT 1
+         )",
+        &[&host],
+    )
+    .await?;
+    tx.execute("SELECT master_remove_node($1, 5432)", &[&host])
+        .await?;
+    tx.commit().await?;
+    Ok(())
 }
